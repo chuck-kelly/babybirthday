4ff17b6ba1f61898fd5b10dc6bb279f5e9be5c7d
chuck <chuck201518@gmail.com>
2022-06-10 13:50:12 -0400

    can make bdays and check for match

--- README.md
+++ README.md
@@ -1,3 +1,9 @@
 # babybirthday
 
-seeing if babys have the same birthday
+How many people need to be in a room before there’s a 50% chance that two of them share the same birthday? 
+
+It's only 23. 
+
+And to make it 97 you need 50 people. 
+
+This a demonstartion of the statistics shown for the Vsause2 youtube channel. https://www.youtube.com/watch?v=ofTb57aZHZs
--- main.py
+++ main.py
@@ -1,4 +1,27 @@
-test = 'working'
+import random
+from re import T
+
+#makes list of birthday (1-365, for each day in a year)
+def make_babies(number_of_babies):
+    list_of_birthdays = [ random.randint(1,365) for number in range(number_of_babies)]
+    # for number in range(number_of_babies):
+    #     birthday = random.randint(1,365)
+    #     list_of_birthdays.append(birthday)
+    #list_of_birthdays.sort()
+    #print(list_of_birthdays)
+    return list_of_birthdays
+
+def check_list_for_dup(list_to_check):
+    if len(list_to_check) == len( set(list_to_check)):
+        return False #no match
+    else:
+        return True #is a match
+    
+
 
 if __name__ == '__main__':
-    print(test)
+    bool = check_list_for_dup( make_babies(23)) 
+    if bool == True:
+        print('match')
+    else:
+        print('nope')
