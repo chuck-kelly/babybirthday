5c92208faa561ace819a66fde606ad59f7d5ddbd
chuck <chuck201518@gmail.com>
2022-06-11 21:16:15 -0400

    added simple terminal interaction

--- main.py
+++ main.py
@@ -1,8 +1,7 @@
 import random
-from re import T
 
-#makes list of birthday (1-365, for each day in a year)
-def make_babies(number_of_babies):
+#makes list of birthdays (1-365, for each day in a year)
+def make_babies(number_of_babies) -> list :
     list_of_birthdays = [ random.randint(1,365) for number in range(number_of_babies)]
     # for number in range(number_of_babies):
     #     birthday = random.randint(1,365)
@@ -11,17 +10,31 @@ def make_babies(number_of_babies):
     #print(list_of_birthdays)
     return list_of_birthdays
 
-def check_list_for_dup(list_to_check):
+def check_list_for_dup(list_to_check) -> bool :
     if len(list_to_check) == len( set(list_to_check)):
         return False #no match
     else:
         return True #is a match
-    
 
+def welcome_message():
+    print(f"Hello! This program will make babies and tell you if they share any birthdays.")
 
 if __name__ == '__main__':
-    bool = check_list_for_dup( make_babies(23)) 
-    if bool == True:
-        print('match')
+    welcome_message()
+
+    ask_to_make = input('Would you like to see if some babies have the same Birthdays? (y/n):')
+
+    if ask_to_make == 'y':
+
+        number_to_make = int(input('How many would you like to make?(23 for 50%, 50 for 97%):'))
+        bool = check_list_for_dup( make_babies(number_to_make)) 
+
+        if bool == True:
+            print('There is a match!')
+        else:
+            print('No match in this bunch.')
+            
+    elif ask_to_make == 'n':
+        pass
     else:
-        print('nope')
+        print('Invalid Input')
